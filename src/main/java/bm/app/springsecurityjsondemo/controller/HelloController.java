ec0b695be6d300edb701575b253c95ef5ff7c6f7
Bartosz76 <mierzwinski.b@gmail.com>
2022-07-10 13:04:38 +0200

    Added two endpoints, for a regular GET request and a secure one and fixed the problem with recognizing Maven.

---
+++ src/main/java/bm/app/springsecurityjsondemo/controller/HelloController.java
@@ -0,0 +1,20 @@
+package bm.app.springsecurityjsondemo.controller;
+
+import bm.app.springsecurityjsondemo.dto.MessageDto;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public MessageDto hello() {
+        return new MessageDto("Hello!");
+    }
+
+    @GetMapping("/")
+    public MessageDto helloSecured() {
+        return new MessageDto("Hello but secured!");
+    }
+
+}
