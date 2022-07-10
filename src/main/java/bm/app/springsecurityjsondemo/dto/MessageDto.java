55725c1c869c2eba98704e8983568e9e777d58eb
Bartosz76 <mierzwinski.b@gmail.com>
2022-07-10 12:21:41 +0200

    Created a DTO.

---
+++ src/main/java/bm/app/springsecurityjsondemo/dto/MessageDto.java
@@ -0,0 +1,14 @@
+package bm.app.springsecurityjsondemo.dto;
+
+public class MessageDto {
+
+    private String message;
+
+    public MessageDto(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
